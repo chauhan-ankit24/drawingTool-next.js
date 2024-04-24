7a0f85b4994cecb759b19beed410b3314f947db8
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2024-04-24 22:55:41 +0530

    added redux toolkit

--- drawing-tool/src/components/Menu/index.js
+++ drawing-tool/src/components/Menu/index.js
@@ -4,26 +4,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPencil, faEraser, faRotateLeft, faRotateRight, faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
 
 import styles from './index.module.css'
-// import { menuItemClick, actionItemClick } from '@/slice/menuSlice'
-// import { MENU_ITEMS } from '@/constants'
 
+import { menuItemClick, actionItemClick } from '@/slice/menuSlice'
 
+import { MENU_ITEMS } from '@/constants'
 const Menu = () => {
-    // const dispatch = useDispatch()
-    // const activeMenuItem = useSelector((state) => state.menu.activeMenuItem)
-    // const handleMenuClick = (itemName) => {
-    //     dispatch(menuItemClick(itemName))
-    // }
+    const dispatch = useDispatch()
+    const activeMenuItem = useSelector((state) => state.menu.activeMenuItem)
+    const handleMenuClick = (itemName) => {
+        dispatch(menuItemClick(itemName))
+    }
 
-    // const handleActioItemClick = (itemName) => {
-    //     dispatch(actionItemClick(itemName))
-    // }
+    const handleActioItemClick = (itemName) => {
+        dispatch(actionItemClick(itemName))
+    }
     return (
         <div className={styles.menuContainer}>
-            <div className={cx(styles.iconWrapper,)} onClick={() => handleMenuClick()}>
+            <div className={cx(styles.iconWrapper, { [styles.active]: activeMenuItem === MENU_ITEMS.PENCIL })} onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)}>
                 <FontAwesomeIcon icon={faPencil} className={styles.icon} />
             </div>
-            <div className={cx(styles.iconWrapper)} onClick={() => handleMenuClick()}>
+            <div className={cx(styles.iconWrapper, { [styles.active]: activeMenuItem === MENU_ITEMS.ERASER })} onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}>
                 <FontAwesomeIcon icon={faEraser} className={styles.icon} />
             </div>
             <div className={styles.iconWrapper} onClick={() => handleActioItemClick(MENU_ITEMS.UNDO)}>
